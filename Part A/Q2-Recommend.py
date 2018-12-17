ffce50db2d3ea4ce7768608aaee954493f0e57c8
AMalt93 <45927823+AMalt93@users.noreply.github.com>
2018-12-17 05:02:58 +0200

    Removed unnecessary lines
    Removed unnecessary lines

--- Part A/Q2-Recommend.py
+++ Part A/Q2-Recommend.py
@@ -55,54 +55,9 @@ def predict(ratings,neighborhoods, k):
                 recommendation_dict[userId_to_predict].append(tup_rec)
             else:
                 recommendation_dict[userId_to_predict] = [tup_rec]
-
-        
-        
-#        print(movie)
-            
-            
-#        for movie_index in ratings_to_consider[1:]:
-#                for user_index,rating in ratings_to_consider[1:].iterrows():
-#
-#                    recommendation += (similarities[0,iterator])*(ratings_to_consider[movie_index][user_index]-means[:user_index][:1]) / sum_of_similarities
-##                    print(recommendation)
-##                    tuple_to_append = (movie_index,recommendation)
-#                    if userId_to_predict in recommendation_dict:
-#                        recommendation_dict[str(userId_to_predict)].append(((movie_index,recommendation)))
-#                    else:
-#                        recommendation_dict[str(userId_to_predict)]=(movie_index,recommendation)
-#                
-#                
-#        iterator+=1
-        
-        
-#        for movie in ratings_to_consider[1:].iterrows():
-##            print(movie[:])
-#            pass
-#            recommendation+= similarities[0,i]*(ratings_to_consider[movie_index][user_index]-means[:movie[:1]]) / sum_of_similarities
-        
-#        sim=similarities[0,1]
-#        print(sim)
-#        for index,rating in ratings_to_consider[0:]:
-            
-        
-#        recommendation = user_mean + 
-        
-    return ratings_to_consider,means, user_mean, similarities, userId_to_predict, recommendation_dict
-    
-        
-    
-#    for neighborhood in neighborhoods:
-#        
-#        userId_to_predict = neighborhood[0]
-#        user_mean = get_mean_rating(ratings_per_user[userId_to_predict].mean())
-#        
-#        similar_users = neighborhood[1:]
-##        user_mean = get_mean_rating(user_to_predict)
-#        
-#        for similar_user in similar_users:
-#            pass
-#            
+   
+    return recommendation_dict
+         
         
 
 
@@ -110,24 +65,10 @@ ratings = pd.read_csv('data/ratings.csv')
 
 ratings_per_user = ratings.pivot_table(index='userId', columns='movieId', values='rating').fillna(0)
 
-#neighborhoods = pd.read_csv('data/neighbors-k-100-movilens.data')
-#text = file.read()
-#text = json.loads(text)
-#
-##data=json_normalize(text)
-##json_normalize(text)
-##
-#for row in text:
-#    neighbors=json_normalize(text[row])
-#k = len(neighborhoods.columns)
-
 file = open('data/neighbors-k-100-movilens.data','r')
 text = file.read()
 text = json.loads(text)
 
-#data=json_normalize(text)
-#json_normalize(text)
-#
 neighbors = pd.DataFrame(columns=json_normalize(text['0']).columns)
 
 for row in text:
@@ -138,7 +79,7 @@ neighbors+=1
 k=len(neighbors.columns)-1
 
 
-ratings_to_consider,means,user_mean, similarities,userId_to_predict, recommendation_dict = predict(ratings,neighbors, k)
+recommendation_dict = predict(ratings,neighbors, k)
 
 with open('data/recommendation.data', 'w') as fp:
     json.dump(recommendation_dict, fp)
